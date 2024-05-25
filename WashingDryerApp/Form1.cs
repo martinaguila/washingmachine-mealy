0be48de30ede88f40a7e4fcb8800d11789d719aa
martin aguila <martin.luther.aguila@gmail.com>
2024-05-25 16:25:51 +0800

    initial commit

---
+++ WashingDryerApp/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WashingDryerApp
+{
+    public partial class Form1 : Form
+    {
+        private WashingMachine washingMachine;
+
+        public Form1()
+        {
+            InitializeComponent();
+            washingMachine = new WashingMachine();
+            washingMachine.StatusChanged += OnStatusChanged;
+            washingMachine.TimeLeftChanged += OnTimeLeftChanged;
+        }
+
+        private void OnStatusChanged(string status)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<string>(OnStatusChanged), status);
+            }
+            else
+            {
+                textBox1.Text = status;
+            }
+        }
+
+        private void OnTimeLeftChanged(int timeLeft)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<int>(OnTimeLeftChanged), timeLeft);
+            }
+            else
+            {
+                textBox2.Text = timeLeft.ToString();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            washingMachine.StartWashing(15);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            washingMachine.StartDrying(10);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            washingMachine.Stop();
+        }
+    }
+}
---
+++ WashingDryerApp/WashingMachine.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Timers;
+
+public class WashingMachine
+{
+    private Timer timer;
+    public string Status { get; private set; }
+    public int TimeLeft { get; private set; }
+
+    public event Action<string> StatusChanged;
+    public event Action<int> TimeLeftChanged;
+
+    public WashingMachine()
+    {
+        timer = new Timer(1000);
+        timer.Elapsed += OnTimedEvent;
+        Status = "Idle";
+    }
+
+    private void OnTimedEvent(object sender, ElapsedEventArgs e)
+    {
+        if (TimeLeft > 0)
+        {
+            TimeLeft--;
+            TimeLeftChanged?.Invoke(TimeLeft);
+        }
+        else
+        {
+            timer.Stop();
+            Status = "Idle";
+            StatusChanged?.Invoke(Status);
+        }
+    }
+
+    public void StartWashing(int duration)
+    {
+        TimeLeft = duration;
+        Status = "Washing";
+        StatusChanged?.Invoke(Status);
+        timer.Start();
+    }
+
+    public void StartDrying(int duration)
+    {
+        TimeLeft = duration;
+        Status = "Drying";
+        StatusChanged?.Invoke(Status);
+        timer.Start();
+    }
+
+    public void Stop()
+    {
+        timer.Stop();
+        Status = "Stopped";
+        StatusChanged?.Invoke(Status);
+        TimeLeft = 0;
+        TimeLeftChanged?.Invoke(TimeLeft);
+    }
+}
